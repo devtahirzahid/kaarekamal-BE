c559aa67e3b4013028e0c8fcb23d8f2fc6d56719
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-19 23:53:18 +0500

    default domain controller

--- src/api/domain/controllers/domain.js
+++ src/api/domain/controllers/domain.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
- * domain controller
+ * Default created Domain controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::domain.domain');
+module.exports = createCoreController("api::domain.domain");
